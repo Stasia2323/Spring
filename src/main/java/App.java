b9fde29ebfbb3f045441a799df69e8b69c36e080
stasia.23 <Stasia.23@mail.ru>
2022-05-31 17:40:13 +0400

    2/1/2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,22 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat =
+                (Cat) applicationContext.getBean("cat");
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+
+        boolean forHelloWord = bean == bean1;
+        boolean forCat = beanCat == beanCat1;
+        System.out.println("Сравнение бинов HelloWorld: " + forHelloWord);
+        System.out.println("Сравнение бинов Cat: " + forCat);
+
+
         System.out.println(bean.getMessage());
     }
 }
